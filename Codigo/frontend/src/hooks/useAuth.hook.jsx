54e443889d8e7b39edb952b3714e4c46126a1ad1
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-02-24 23:46:36 -0500

    Add useAuth hook for authentication

---
+++ Codigo/frontend/src/hooks/useAuth.hook.jsx
@@ -0,0 +1,47 @@
+import {createContext, useContext, useMemo} from "react";
+import {useNavigate} from "react-router-dom";
+import {useLocalStorage} from "./useLocalStorage.hook";
+import PropTypes from "prop-types";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+  const [user, setUser] = useLocalStorage("user", null);
+  const navigate = useNavigate();
+
+  // call this function when you want to authenticate the user
+  const login = async (data) => {
+    setUser(data);
+    navigate("/admin", {replace: true});
+  };
+
+  // call this function to sign out logged in user
+  const logout = () => {
+    setUser(null);
+    navigate("/auth/login");
+  };
+
+  const getToken = () => {
+    const value = window.localStorage.getItem("user");
+    return JSON.parse(value);
+  };
+
+  const value = useMemo(
+    () => ({
+      user,
+      login,
+      logout,
+      getToken,
+    }),
+    [user]
+  );
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
