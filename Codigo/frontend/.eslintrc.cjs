cf3a3fd5aa0547e3f47f041b8172f45b3fa15989
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-02-24 23:48:22 -0500

    Add __dirname as readonly global variable in .eslintrc.cjs

--- Codigo/frontend/.eslintrc.cjs
+++ Codigo/frontend/.eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  globals: {
+    __dirname: "readonly",
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
