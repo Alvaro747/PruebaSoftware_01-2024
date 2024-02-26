7194cd5dbe4db9f8e58acb9381c5df6d03beb776
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-02-25 22:12:36 -0500

    Update routing and add chart component styles

--- Codigo/frontend/src/App.jsx
+++ Codigo/frontend/src/App.jsx
@@ -1,32 +1,20 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {AuthProvider} from "./hooks/useAuth.hook";
-import "./App.css";
+import {LayoutAuth} from "@layouts";
+import {Login, Register, Home} from "@pages";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<></>}>
-            <Route index element={<></>} />
+          <Route path="/" element={<LayoutAuth />} />
+          <Route path="/auth">
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Route>
-          <Route path="/auth" element={<></>}>
-            <Route index element={<></>} />
-            <Route path="login" element={<></>} />
-            <Route path="logout" element={<></>} />
-            <Route path="lock-screen" element={<></>} />
-          </Route>
-          <Route path="/admin" element={<></>}>
-            <Route index element={<></>} />
-            <Route path="account">
-              <Route index path="overview" element={<></>} />
-              <Route path="profile" element={<></>} />
-            </Route>
-
-            <Route path="user">
-              <Route path="management" element={<></>} />
-              <Route path="detail/:id" element={<></>} />
-            </Route>
+          <Route path="/dashboard" /* element={<LayoutAuth />} */>
+            <Route index element={<Home />} />
 
             <Route path="*" element={<></>} />
           </Route>
