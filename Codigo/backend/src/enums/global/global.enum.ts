8688f951de1ae946a392ed35a21b363c634e2721
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-02-25 23:41:12 -0500

    Add StatusEnum and StatusEnumType to global.enum.ts

---
+++ Codigo/backend/src/enums/global/global.enum.ts
@@ -0,0 +1,8 @@
+export enum StatusEnum {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  SUSPENDED = 'suspended',
+  DELETED = 'deleted',
+}
+
+export type StatusEnumType = 'active' | 'inactive' | 'suspended' | 'deleted';
