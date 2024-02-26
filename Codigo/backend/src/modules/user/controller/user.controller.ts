5a005aaf176a54462e850a74558a11cffa2e5c88
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-02-25 23:40:56 -0500

    Add UserModule, UserController, and UserPrismaRepository

---
+++ Codigo/backend/src/modules/user/controller/user.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  BadRequestException,
+} from '@nestjs/common';
+import { UserService } from '@modules/user/service/user.service';
+import { CreateUserDto } from '@modules/user/dto/create-user.dto';
+import { UpdateUserDto } from '@modules/user/dto/update-user.dto';
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get(':queryselector')
+  async get(@Param('queryselector') queryselector: string, @Query('search') search: string) {
+    return this.userService.get({ queryselector, search });
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    if (!id) {
+      throw new BadRequestException(`please provide an id to update`);
+    }
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    if (!id) {
+      throw new BadRequestException(`please provide an id to delete`);
+    }
+
+    return this.userService.delete(id);
+  }
+}
